74c54a52fb4ab24884f7ff57803591f73f26e082
ronald <ronald_wang@htc.com>
2018-08-30 08:38:21 +0800

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  @include('shared.head')
+  <body>
+    <div id="app">
+      {{ $msg }} 
+    </div>
+  </body>
+</html>
+
---
+++ resources/views/shared/head.blade.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ $title }}</title>
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+</head>
