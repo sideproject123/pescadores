5de54f457f451a7cac7424490a30b52b21cb3507
ronald_wang <ronald_wang@htc.com>
2018-09-11 00:13:54 +0800

    add header

---
+++ resources/lang/zh-tw/site.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'online_buy_ticket' => '線上購票',
+  'route_n_fare' => '航班與票價',
+];
+
--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,3 +1,14 @@
 <div id="header">
-  <span>this is header</span>
+  <div class="logo-wrap">
+    <img class="logo" src="../image/logo.png" />
+    <img class="logo-name" src="../image/logo_name.png" />
+  </div>
+  <ul class="link-wrap">
+    <li class="link">
+      <a href="">{{ __('site.online_buy_ticket') }}</a>
+    </li>
+    <li class="link">
+      <a href="">{{ __('site.route_n_fare') }}</a>
+    </li>
+  </ul>
 </div>
