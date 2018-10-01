5ea9d461d083c9c79317ee826d852d0faf0d4be0
Ronald Wang(王熙暉) <ronald_wang@htc.com>
2018-10-01 11:03:26 +0800

    updates

--- resources/views/shared/cruise/routes_table.blade.php
+++ resources/views/shared/cruise/routes_table.blade.php
@@ -21,11 +21,12 @@
       <td data-cell-key="time">{{ substr($d->datetime, 11, 5) }}</td>
       <td data-cell-key="ferry">{{ $d->ferryName }}</td>
       <td data-cell-key="remain">remain</td>
-      <td data-cell-key="function" class="actions">
+      <td data-cell-key="actions" class="actions">
         <button type="text" data-action="activate" data-id="{{ $d->id }}" class="btn activate">啟用</button>
         <button type="text" data-action="delete" data-id="{{ $d->id }}" class="btn delete">刪除</button>
         <button type="text" data-action="cancel" data-id="{{ $d->id }}" class="btn cancel">停售</button>
         <button type="text" data-action="reserve" data-id="{{ $d->id }}" data-fid="{{ $d->ferryId }}" class="btn reserve">劃位</button>
+        <button type="text" data-action="refund" data-id="{{ $d->id }}" class="btn refund">退票</button>
       </td>
     </tr>      
   @endforeach
